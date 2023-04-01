db0167b7bbc07bbae977113d6f4aa92cbdfc0bd6
Jorshstar <ajosh22.aj@gmail.com>
2023-04-01 08:50:40 -0700

    networking

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Module that sends a request to a URL and displays the value of the X-Request-Id header variable"""
-
+"""
+Python script that takes in a URL, sends a request to the URL and displays the
+value of the variable X-Request-Id in the response header
+"""
 import requests
 import sys
 
