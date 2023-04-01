d6af4ee8363e3d52c21beeb066fe48c044009f65
Jorshstar <ajosh22.aj@gmail.com>
2023-04-01 09:25:45 -0700

    networking

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,31 +7,23 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 import requests
 import sys
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         if sys.argv[1].isalpha():
             q = sys.argv[1]
         else:
-            print("No result")
-            exit()
+            q = ""
     else:
         q = ""
 
-    url = "http://0.0.0.0:5000/search_user"
     try:
-        r = requests.post(url, data={"q": q})
-        r.raise_for_status()  # Check if the request was successful
+        response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+        json_dict = response.json()
 
-        json_dict = r.json()
-        if len(json_dict) == 0:
-            print("No result")
+        if len(json_dict) > 0:
+            print("[{}] {}".format(json_dict.get("id"), json_dict.get("name")))
         else:
-            user_id = json_dict.get("id")
-            name = json_dict.get("name")
-            print("[{}] {}".format(user_id, name))
-    except requests.exceptions.HTTPError as e:
-        print("HTTP error: {}".format(e))
-    except requests.exceptions.JSONDecodeError:
+            print("No result")
+    except ValueError:
         print("Not a valid JSON")
-    except Exception as e:
-        print("An error occurred: {}".format(e))
