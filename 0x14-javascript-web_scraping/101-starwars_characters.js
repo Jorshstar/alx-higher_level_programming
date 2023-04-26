487166441b3ad44b9ca3bdefb47252466c486c31
Jorshstar <Jorshstar>
2023-04-25 18:41:01 -0700

    javascript

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,38 +1,42 @@
 #!/usr/bin/node
-
 const request = require('request');
-const movieId = process.argv[2];
 
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/' + movieId + '/';
-const characters = [];
-
-function getCharacterName(characterUrl) {
-  return new Promise((resolve, reject) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const character = JSON.parse(body);
-        resolve(character.name);
-      }
-    });
-  });
+if (process.argv.length !== 3) {
+  console.error('Usage: ./101-starwars_characters.js <movie_id>');
+  process.exit(1);
 }
 
-request(apiUrl, function (error, response, body) {
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
-    const movie = JSON.parse(body);
-    const promises = movie.characters.map((characterUrl) => {
-      return getCharacterName(characterUrl);
-    });
-    Promise.all(promises)
-      .then((names) => {
-        console.log(names.join('\n'));
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    process.exit(1);
   }
+
+  if (response.statusCode !== 200) {
+    console.error(`Unexpected status code: ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error(`Unexpected status code: ${response.statusCode}`);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
 });
